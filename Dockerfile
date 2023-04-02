2c8156668b42493d44a6ae2bab5a4baaec639557
Bruno de Araujo Alves <me@baraus.dev>
2023-04-02 18:30:08 -0300

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,22 @@
-FROM ubuntu:22.04
+FROM nvidia/cuda:11.2.0-base-ubuntu18.04
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
     wget \
     curl \
     git \
     screen \
-    nvtop \
-    glances \
     openssh-server \
     ca-certificates \
     && rm -rf /var/lib/apt/lists/*
 
-RUN curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh 
-RUN bash miniconda.sh -b 
-RUN rm miniconda.sh
+RUN wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
+    /bin/bash ~/miniconda.sh -b -p /opt/conda && \
+    rm ~/miniconda.sh
 
-ENV PATH="/root/miniconda3/bin:${PATH}"
+ENV PATH /opt/conda/bin:$PATH
 
 COPY install.sh /
 
 RUN chmod +x /install.sh
-
-RUN bash -i /install.sh
 
 CMD ["/bin/bash", "-c", "service ssh start; /bin/bash"] 
--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+USER=${USER:-"dnn"}
+PASS=${PASS:-"dnn"}
+
+# if not user was created, create one
+useradd -m -s /bin/bash -p $(openssl passwd -1 $PASS) $USER
+usermod -aG sudo $USER
+chown -R $USER:$USER /opt/conda
+
+echo $PASS | su - $USER << EOF
+conda update -n base -c defaults conda -y && \
 conda create -n dnn python=3.10 -y && \
 
 conda init && \
@@ -10,15 +20,17 @@ conda install -n dnn -c conda-forge cudatoolkit=11.2.2 -y && \
 conda install -n dnn -c conda-forge cudnn -y && \
 conda install -n dnn -c nvidia cuda-nvcc -y && \
 
-echo "export XLA_FLAGS=--xla_gpu_cuda_data_dir=/root/miniconda3/envs/dnn/" >> ~/.bashrc && \
+echo "export XLA_FLAGS=--xla_gpu_cuda_data_dir=/opt/conda/envs/dnn/" >> ~/.bashrc && \
 
 # Install common packages
-pip install tensorflow==2.11.*
-pip install matplotlib
-pip install numpy
-pip install pandas
-pip install scikit-learn
-pip install scipy
-pip install seaborn
-pip install tqdm
+pip install tensorflow==2.11.* && \
+pip install matplotlib && \
+pip install numpy && \
+pip install pandas && \
+pip install scikit-learn && \
+pip install scipy && \
+pip install seaborn && \
+pip install tqdm && \
 pip install jupyter
+
+EOF
